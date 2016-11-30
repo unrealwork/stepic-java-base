16d0bc837211a6f3e3d601becd76a1a00c975098
Unrealwork <unrealwork@gmail.com>
2016-11-30 12:38:23 +0300

    end chapter 5

--- src/main/java/org/stepic/java/io/serialization/Serialization.java
+++ src/main/java/org/stepic/java/io/serialization/Serialization.java
@@ -4,9 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
-/**
- * Created by shmagrinskiy on 11/29/16.
- */
+
 public class Serialization {
     public static Animal[] deserializeAnimalArray(byte[] data) {
         try {
